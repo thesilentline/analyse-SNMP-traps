2a9ecf9e8229ed981ceaae8a0089b07dbe80cbbb
Divyanshi Singh Bora <82605925+thesilentline@users.noreply.github.com>
2023-07-23 18:57:33 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -2,37 +2,67 @@ import socket
 import json
 import logging
 from trap_mail import generate_mail
-from data_processing import decoding_snmp_traps
+from data_processing import *
 from database import store_trap_messages
+import threading
+import tkinter as tk
+from PIL import ImageTk, Image
 
-ip_address = ''
-port = 164
 
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def test_func(entry):
+    output_text.insert("end", entry + "\n")
 
-s.bind((ip_address, port))
 
 # to process snmp traps
 def process_snmp_traps(trap_data):
-    oid_value_pairs = trap_data.split(" ")[9:]
-    print(oid_value_pairs[1])
-    trap_oid = oid_value_pairs[1].split("=")[1]
 
+    details, oid_pair_dictionary = decode_message(trap_data)
+    details_list = details.split(" ")
+
+
+    trap_oid = oid_pair_dictionary[".1.3.6.1.6.3.1.1.4.1.0"]
+    
     message = decoding_snmp_traps(trap_oid)
-
-
-
-    print(f"Message: {message}")
-    generate_mail(message)
-    store_trap_messages(message)
 
     logging.basicConfig(
     filename='snmp_traps.log',
     level=logging.DEBUG,
     format='%(asctime)s  %(levelname)-8s: %(process)d %(thread)d %(message)s')
     logger = logging.getLogger("mylogger")
-    logger.info(format(message))
+
+    if(trap_oid == ".1.3.6.1.4.1.9.9.513.0.11"):
+        logger.warning(format(message))
+        location= oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"] 
+        message = f"{message} the router in the {location} has gone DOWN. Please check the router. Thank you!"
+        generate_mail(message)
+        test_func(">>> Email sent!")
+        print(oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"])
+    elif(trap_oid == ".1.3.6.1.4.1.9.9.599.0.6"):
+        logger.info(format(message))
+        temp= "CLIENT DISSOCIATION:"
+        message = f"{temp} {message}"
+        generate_mail(message)
+        test_func(">>> Email sent!")
+    elif(trap_oid == ".1.3.6.1.4.1.9.9.599.0.7"):
+        logger.info(format(message))
+        temp= "CLIENT ASSOCIATION:"
+        message = f"{temp} {message}"
+        generate_mail(message)
+        test_func(">>> Email sent!")
+    elif(trap_oid == ".1.3.6.1.4.1.9.9.599.0.9"):
+        logger.info(format(message))
+        temp= "Station establishes a connection or association with a controller."
+        message = f"{temp} {message}"
+        generate_mail(message)
+        test_func(">>> Email sent!")
+    else:
+        logger.info(format(message))
+
+    print(f"Message: {message}")
+    test_func(f"Message: {message}")
+    test_func("----------------------------------------------")
+
+
 
 
 def receiving_snmp_traps():
@@ -40,6 +70,7 @@ def receiving_snmp_traps():
     while True:
 
         print(f">>> Socket bound to {ip_address}:{port}")
+        test_func(f">>> Socket bound to {ip_address}:{port}")
 
         # receive data from the client
         s.listen(5)
@@ -47,6 +78,7 @@ def receiving_snmp_traps():
         # accept connections from outside
         c, address = s.accept()
         print(f">>> Connection from {address} has been established!")
+        test_func(">>> Connection from has been established!")
 
         # # Set a timeout of 10 seconds
 
@@ -64,9 +96,10 @@ def receiving_snmp_traps():
 
         except socket.timeout:
             print(">>> Socket timeout")
+            test_func(">>> Socket timeout")
 
         decode_data = data.decode('utf-8').strip()
-        print(f"Data received from client: {repr(decode_data)}")       
+        print(f"Data received from client: {repr(decode_data)}")        
 
         # convert the recieved data JSON array into python list 
         if decode_data.strip():
@@ -76,12 +109,82 @@ def receiving_snmp_traps():
                 process_snmp_traps(array_data)
             except json.JSONDecodeError:
                 print(">>> Invalid JSON format received")
+                test_func(">>> Invalid JSON format received")
+                test_func("----------------------------------------------")
         else:
             print(">>> Empty data received")
+            test_func(">>> Empty data received")
+            test_func("----------------------------------------------")
 
         # close the connection and socket
         c.close()
         # receiving_snmp_traps()
 
-while True:
-    receiving_snmp_traps()
+
+# def start_snmp_trap_receiver():
+#     # Start the SNMP trap receiver in a separate thread
+#     # while True:
+
+def start_snmp_trap_receiver():
+    receiving_snmp_thread = threading.Thread(target=receiving_snmp_traps)
+    receiving_snmp_thread.start()
+
+
+HEIGHT = 590
+WIDTH = 590
+
+def show_snmp_logs():
+    try:
+        with open('snmp_traps.log', 'r') as log_file:
+            log_contents = log_file.read()
+            output_text.delete(1.0, tk.END)  # Clear the existing content
+            output_text.insert(tk.END, log_contents)
+    except FileNotFoundError:
+        output_text.delete(1.0, tk.END)
+        output_text.insert(tk.END, "snmp_traps.log not found!")
+
+def clear_screen():
+    output_text.delete(1.0, tk.END)
+
+root = tk.Tk()
+root.title("SNMP Trap Receiver")
+
+# Create the canvas
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+image = Image.open('newdelgihxgo.jpeg')
+background_image = ImageTk.PhotoImage(image)
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+output_frame = tk.Frame(root, bg="white")
+output_frame.place(relwidth=0.85, relheight=0.7, relx=0.5, rely=0.05, anchor="n")
+
+output_text = tk.Text(output_frame, bg="black", fg="white")
+output_text.pack(fill="both", expand=True, padx=5, pady=5)
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.85, relheight=0.1, relx=0.5, rely=0.8, anchor="n")
+
+button_snmp_listener = tk.Button(frame, text="SNMP listener", bg="white", fg="black", command=start_snmp_trap_receiver)
+button_snmp_listener.place(relx=0.025, rely=0.1, relwidth=0.3, relheight=0.8)
+
+button_show_logs = tk.Button(frame, text="Show Logs", bg="white", fg="black", command=show_snmp_logs)
+button_show_logs.place(relx=0.35, rely=0.1, relwidth=0.3, relheight=0.8)
+
+button_clear_screen = tk.Button(frame, text="Clear Screen", bg="white", fg="black", command=clear_screen)
+button_clear_screen.place(relx=0.675, rely=0.1, relwidth=0.3, relheight=0.8)
+
+
+ip_address = ''
+port = 164
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((ip_address, port))
+
+
+# Start the tkinter main event loop
+root.mainloop()
