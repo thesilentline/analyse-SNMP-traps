021a6075cd9cf079d41468bcfed0b14251012f76
Divyanshi Singh Bora <82605925+thesilentline@users.noreply.github.com>
2023-07-29 01:23:55 +0530

    Update and rename receiver.py to main.py

--- main.py
+++ main.py
@@ -7,7 +7,28 @@ from database import store_trap_messages
 import threading
 import tkinter as tk
 from PIL import ImageTk, Image
+from logupdate import *
 
+snmp_formatter = logging.Formatter('%(asctime)s  %(levelname)-8s: %(process)d %(thread)d %(message)s')
+status_formatter = logging.Formatter('%(message)s')
+
+def setup_logger(n, name, log_file, level=logging.INFO):
+    """To setup as many loggers as you want"""
+
+    handler = logging.FileHandler(log_file)
+    if(n==1):      
+        handler.setFormatter(snmp_formatter)
+    else:
+        handler.setFormatter(status_formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+logger = setup_logger(1, 'first_logger', 'snmp_traps.log')
+super_logger = setup_logger(2, 'second_logger', 'status_info.log')
 
 def test_func(entry):
     output_text.insert("end", entry + "\n")
@@ -19,24 +40,42 @@ def process_snmp_traps(trap_data):
     details, oid_pair_dictionary = decode_message(trap_data)
     details_list = details.split(" ")
 
+    timesnmpreceived = details_list[0]
+    senderip = details_list[2]
+
 
     trap_oid = oid_pair_dictionary[".1.3.6.1.6.3.1.1.4.1.0"]
     
     message = decoding_snmp_traps(trap_oid)
 
-    logging.basicConfig(
-    filename='snmp_traps.log',
-    level=logging.DEBUG,
-    format='%(asctime)s  %(levelname)-8s: %(process)d %(thread)d %(message)s')
-    logger = logging.getLogger("mylogger")
+
 
     if(trap_oid == ".1.3.6.1.4.1.9.9.513.0.11"):
+
         logger.warning(format(message))
-        location= oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"] 
-        message = f"{message} the router in the {location} has gone DOWN. Please check the router. Thank you!"
-        generate_mail(message)
+        location= oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"]
+        present,row = findlocation(location)
+        if(present):
+            delete_row_from_log_file(row)
+        else:
+            super_logger.warning(f"{location} {timesnmpreceived} {senderip} DOWN")
+        message = f"{message} The router in the {location} has gone DOWN. Please check the router. Thank you!"
+        # generate_mail(message)
         test_func(">>> Email sent!")
         print(oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"])
+
+    elif(trap_oid == ".1.3.6.1.4.1.9.9.513.0.10"):
+
+        logger.warning(format(message))
+        location= oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"]
+        present,row = findlocation(location)
+        if(present):
+            delete_row_from_log_file(row)
+        message = f"{message} The router in the {location} is fine now. Thank you!"
+        # generate_mail(message)
+        test_func(">>> Email sent!")
+        print(oid_pair_dictionary[".1.3.6.1.4.1.9.9.513.1.1.1.1.5.0"])
+
     elif(trap_oid == ".1.3.6.1.4.1.9.9.599.0.6"):
         logger.info(format(message))
         temp= "CLIENT DISSOCIATION:"
@@ -143,6 +182,16 @@ def show_snmp_logs():
         output_text.delete(1.0, tk.END)
         output_text.insert(tk.END, "snmp_traps.log not found!")
 
+def show_statusinfo():
+    try:
+        with open('status_info.log', 'r') as log_file:
+            log_contents = log_file.read()
+            output_text.delete(1.0, tk.END)  # Clear the existing content
+            output_text.insert(tk.END, log_contents)
+    except FileNotFoundError:
+        output_text.delete(1.0, tk.END)
+        output_text.insert(tk.END, "statusinfo.log not found!")
+
 def clear_screen():
     output_text.delete(1.0, tk.END)
 
@@ -159,23 +208,25 @@ background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
 output_frame = tk.Frame(root, bg="white")
-output_frame.place(relwidth=0.85, relheight=0.7, relx=0.5, rely=0.05, anchor="n")
+output_frame.place(relwidth=0.85, relheight=0.75, relx=0.5, rely=0.05, anchor="n")
 
 output_text = tk.Text(output_frame, bg="black", fg="white")
 output_text.pack(fill="both", expand=True, padx=5, pady=5)
 
-frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.85, relheight=0.1, relx=0.5, rely=0.8, anchor="n")
+button_frame = tk.Frame(root, bg="white")
+button_frame.place(relwidth=0.85, relheight=0.15, relx=0.5, rely=0.82, anchor="n")
 
-button_snmp_listener = tk.Button(frame, text="SNMP listener", bg="white", fg="black", command=start_snmp_trap_receiver)
-button_snmp_listener.place(relx=0.025, rely=0.1, relwidth=0.3, relheight=0.8)
+button_snmp_listener = tk.Button(button_frame, text="SNMP listener", bg="white", fg="black", command=start_snmp_trap_receiver)
+button_snmp_listener.place(relx=0.015, rely=0.07, relwidth=0.48, relheight=0.4)
 
-button_show_logs = tk.Button(frame, text="Show Logs", bg="white", fg="black", command=show_snmp_logs)
-button_show_logs.place(relx=0.35, rely=0.1, relwidth=0.3, relheight=0.8)
+button_show_logs = tk.Button(button_frame, text="Show Logs", bg="white", fg="black", command=show_snmp_logs)
+button_show_logs.place(relx=0.51, rely=0.07, relwidth=0.48, relheight=0.4)
 
-button_clear_screen = tk.Button(frame, text="Clear Screen", bg="white", fg="black", command=clear_screen)
-button_clear_screen.place(relx=0.675, rely=0.1, relwidth=0.3, relheight=0.8)
+button_clear_screen = tk.Button(button_frame, text="Clear Screen", bg="white", fg="black", command=clear_screen)
+button_clear_screen.place(relx=0.015, rely=0.54, relwidth=0.48, relheight=0.4)
 
+button_status = tk.Button(button_frame, text="Status Info", bg="white", fg="black", command=show_statusinfo)
+button_status.place(relx=0.51, rely=0.54, relwidth=0.48, relheight=0.4)
 
 ip_address = ''
 port = 164
